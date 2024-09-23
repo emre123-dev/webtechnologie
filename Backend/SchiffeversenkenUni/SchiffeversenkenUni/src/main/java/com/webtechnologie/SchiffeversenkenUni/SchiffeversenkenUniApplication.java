869b761c5a6f930e5928d2b83d3949e66b246bf0
emre <emre1cola@gmail.com>
2024-09-23 22:52:40 +0200

    projekterstellt

---
+++ Backend/SchiffeversenkenUni/SchiffeversenkenUni/src/main/java/com/webtechnologie/SchiffeversenkenUni/SchiffeversenkenUniApplication.java
@@ -0,0 +1,13 @@
+package com.webtechnologie.SchiffeversenkenUni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchiffeversenkenUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchiffeversenkenUniApplication.class, args);
+	}
+
+}
---
+++ Backend/SchiffeversenkenUni/SchiffeversenkenUni/src/test/java/com/webtechnologie/SchiffeversenkenUni/SchiffeversenkenUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtechnologie.SchiffeversenkenUni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchiffeversenkenUniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
